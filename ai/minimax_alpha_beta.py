5e3dde5a46ea6265881a95f4b40a43e2792c3cd9
Saifullah-1 <siafmosad@gmail.com>
2024-11-29 19:15:59 +0200

    minimax with alpha beta pruning

---
+++ ai/minimax_alpha_beta.py
@@ -0,0 +1,44 @@
+from service.Service import *
+
+class MinimaxAlphaBeta:
+  def __init__(self, turn = '1'):
+    self.turn = turn
+    self.minimax_tree = dict()
+  
+  def maximize(self, state, alpha, beta, K):
+    # terminal state
+    if K == 0:
+      return (None, eval(state, self.turn))
+    
+    max_child = None
+    max_utility = -float('inf')
+
+    for child in get_children(state, self.turn):
+      _, utility = self.minimize(child, alpha, beta, K - 1)
+      if utility > max_utility:
+        max_utility = utility
+        max_child = child
+
+      if max_utility >= beta:
+        break
+    
+    return (max_child, max_utility)
+  
+  def minimize(self, state, alpha, beta, K):
+    # terminal state
+    if K == 0:
+      return (None, eval(state, self.turn))
+    
+    min_child = None
+    min_utility = float('inf')
+
+    for child in get_children(state, '2' if self.turn == '1' else '1'):
+      _, utility = self.maximize(child, alpha, beta, K - 1)
+      if utility < min_utility:
+        min_utility = utility
+        min_child = child
+      
+      if min_utility <= alpha:
+        break
+    
+    return (min_child, min_utility)
