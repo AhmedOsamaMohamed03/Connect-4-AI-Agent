68f7a1fa4415ae4dcbe9f51bc95cb3fa47d58777
Saifullah-1 <siafmosad@gmail.com>
2024-11-30 01:19:05 +0200

    fix bugs

--- ai/Minimax.py
+++ ai/Minimax.py
@@ -1,38 +1,43 @@
 from service.Service import *
 
+
 class Minimax:
-  def __init__(self, turn = '0'):
-    self.turn = turn
-    self.minimax_tree = dict()
-  
-  def maximize(self, state, K):
-    # terminal state
-    if K == 0:
-      return (None, eval(state, self.turn))
-    
-    max_child = None
-    max_utility = -float('inf')
+    def __init__(self, turn='0'):
+        self.turn = turn
+        self.minimax_tree = list()
 
-    for child in get_children(state, self.turn):
-      _, utility = self.minimize(child, K - 1)
-      if utility > max_utility:
-        max_utility = utility
-        max_child = child
-    
-    return (max_child, max_utility)
-  
-  def minimize(self, state, K):
-    # terminal state
-    if K == 0:
-      return (None, eval(state, self.turn))
-    
-    min_child = None
-    min_utility = float('inf')
+    def maximize(self, state, K):
+        # terminal state
+        if K == 0:
+            return None, eval(state)
 
-    for child in get_children(state, str(1 - int(self.turn))):
-      _, utility = self.maximize(child, K - 1)
-      if utility < min_utility:
-        min_utility = utility
-        min_child = child
-    
-    return (min_child, min_utility)
+        max_child = None
+        max_utility = -float('inf')
+
+        for child in get_children(state, self.turn):
+            _, utility = self.minimize(child, K - 1)
+            if utility > max_utility:
+                max_utility = utility
+                max_child = child
+
+        return max_child, max_utility
+
+    def minimize(self, state, K):
+        # terminal state
+        if K == 0:
+            return None, eval(state)
+
+        min_child = None
+        min_utility = float('inf')
+
+        for child in get_children(state, str(1 - int(self.turn))):
+            _, utility = self.maximize(child, K - 1)
+            if utility < min_utility:
+                min_utility = utility
+                min_child = child
+
+        return min_child, min_utility
+
+    def print_minimax_tree(self, state):
+        # TODO -> printing the minimax tree
+        pass
