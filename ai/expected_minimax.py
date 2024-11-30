a99f0d40c28a6f410aeabf6d68ef15c78422e02c
AhmedOsama <aosama54540991@gmail.com>
2024-11-30 19:37:40 +0200

    Update expectiminimax

--- ai/expected_minimax.py
+++ ai/expected_minimax.py
@@ -1,9 +1,8 @@
 from service.Service import*
 
 class Expectiminimax:
-    def _init_(self, turn = '0', chosen_column = 1):
+    def _init_(self, turn = '0'):
         self.turn = turn
-        self.chosen_column = chosen_column
 
     def maximize(self, state, k):
         if k == 0:
@@ -12,27 +11,27 @@ class Expectiminimax:
         max_child = None
         max_utility = -float("inf")
 
-        for child, prob in get_children_probability(state, self.turn, self.chosen_column ):
-            _, utility = self.minimize(child, k - 2)
+        for child, chosen_column in get_children(state, str(1-int(self.turn))):
+            _, utility = self.minimize(child, k - 1, chosen_column)
             
-            if prob * utility > max_utility:
-                max_utility = prob * utility
+            if utility > max_utility:
+                max_utility = utility
                 max_child = child
 
         return  (max_child, max_utility)
 
-    def minimize(self, state, k):
+    def minimize(self, state, k, chosen_column):
         if k == 0:
             return (None, eval(state, self.turn))
         
         min_child = None
         min_utility = float("inf")
 
-        for child, prob in get_children(state, self.turn):
-            _, utility = self.maximize(child, k - 1)
+        for child, prob in get_children_probability(state, str(1-int(self.turn)), chosen_column):
+            _, utility = self.maximize(child, k - 2)
             
-            if utility < min_utility:
-                min_utility = utility
+            if prob * utility < min_utility:
+                min_utility = prob * utility
                 min_child = child
                 
         return  (min_child, min_utility)
